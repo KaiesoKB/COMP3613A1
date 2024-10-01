da0ad93034dd2fc1418c49101460e7c038fff4b0
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-10-01 15:55:01 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,10 @@ from App.database import db, get_migrate
 from App.models import User
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
+from App.models import Student 
+from App.models import Student_reviews
+from App.controllers import (
+    
 
 
 # This commands file allow you to create convenient CLI commands for testing controllers
