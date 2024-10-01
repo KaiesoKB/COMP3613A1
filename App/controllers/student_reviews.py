93a1cefae2703c53cb06fa0367e74fa6035694e5
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-09-30 22:50:00 -0400

    Update student_reviews.py

--- App/controllers/student_reviews.py
+++ App/controllers/student_reviews.py
@@ -14,10 +14,11 @@ def view_student_reviews(student_id):
   if not Student.query.get(student_id):
     return {"message": "student does not exist"}
 
-  if not Student_reviews.query.filter_by(student_id = student_id).all():
+  student_reviews = Student_reviews.query.filter_by(student_id = student_id).all():
+  if not student_reviews:
     return {"message": "This student has no reviews"}
 
   review_list = []
-  for review in reviews:
+  for review in student_reviews:
       review_list.append(review.get_json())
   return {"reviews": review_list}
