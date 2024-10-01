e6143772be99105b1e0b6b372763d00e97564292
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-09-30 22:20:17 -0400

    Update student_review.py

--- App/models/student_review.py
+++ App/models/student_review.py
@@ -1,2 +1,22 @@
 from App.database import db
 from .user import User
+
+class Student_review(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    student_id = db.Column(db.Integer, db.ForeignKey(Student.student_id), nullable = False)
+    staff_id = db.Column(db.Integer, db.ForeignKey(Staff.staff_id), nullable = False)
+    review_description = db.Column(db.String(255), nullable = False)
+    rating = db.Column(db.Integer, nullable = False)
+
+    def validate_rating(self, key, rating):
+        if rating < 0 or rating > 5:
+            raise ValueError("Rating must be between 0 and 5")
+        return rating
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'staff_id': self.staff_id,
+            'review_description': self.review_description,
+            'rating': self.rating
+        }
