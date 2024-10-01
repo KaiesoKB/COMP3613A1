cf43b818dae0d8177267e3a88680a729162024c1
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-09-30 22:33:33 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -1,1 +1,12 @@
+def add_student(student_id, student_name):
+  if Student.query.filter_by(student_id=student_id).first():
+    return {"message": "Student already exists."}
 
+  new_student = Student(student_id = student_id, name = student_name)
+  db.session.add(new_student)
+  db.session.commit()
+  return {"message": f"student {student_name| has been added."}
+
+
+
+    
