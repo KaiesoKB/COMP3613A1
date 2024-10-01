22b6f5e2b046f7c18632475aabd8a3b3c04c0bfd
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-09-30 22:14:22 -0400

    student.py

--- App/models/student.py
+++ App/models/student.py
@@ -1,1 +1,20 @@
 
+
+class Student(db.Model):
+    student_id = db.Column(db.Integer, primary_key = True, unique = True)
+    name = db.Column(db.string(30), nullable = False)
+    reviews = db.relationship('Review', backred = 'student')
+
+    def __init__(self, student_id, name):
+        self.student_id = student_id
+        self.name = name
+
+    def get_json(self):
+        return {
+            'student_id': self.student_id,
+            'name': self.name,
+            review_list = []
+            for review in self.reviews:
+                reviews_list.append(review.get_json())
+            'reviews': reviews_list
+        }
