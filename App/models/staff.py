765899d13ae2aca72859ddd5be593b5aba3ee20b
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-09-30 22:19:27 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,1 +1,13 @@
+class Staff(db.Model):
+    staff_id = db.Colmumn(db.Integer, primary key = True, unique = True)
+    name = db.Column(db.String(30), nullable=False)
 
+    def __init__(self, staff_id, name):
+        self.staff_id = staff.id
+        self.name = name
+
+    def get_json(self):
+        return {
+            'staff_id': self.staff_id,
+            'name': self.name
+        }
