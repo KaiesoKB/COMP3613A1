c5b838d3dd36e00f8a39715c411e6de6a6ab4532
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-10-01 23:01:53 -0400

    Update student_reviews.py

--- App/controllers/student_reviews.py
+++ App/controllers/student_reviews.py
@@ -1,15 +1,6 @@
 from App.models import Student_reviews
 from App.database import db
 
-def add_review(student_id, staff_id, review_description, rating):
-  if not Student.query.get(student_id):
-    return {"message": "student does not exist"}
-
-  new_review = Review(student_id = student_id, staff_id = staff_id, review_description = review_description, rating = rating)
-  db.session.add(new_review)
-  db.session.commit()
-  return {"message": "review has been added"}
-
 def view_student_reviews(student_id):
   if not Student.query.get(student_id):
     return {"message": "student does not exist"}
