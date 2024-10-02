ced0032495cb64b84d49228165ba44b32a0e86e2
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2024-10-01 22:58:44 -0400

    Update staff.py

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -1,1 +1,10 @@
 
+
+def add_review(student_id, staff_id, review_description, rating):
+  if not Student.query.get(student_id):
+    return {"message": "student does not exist"}
+
+  new_review = Review(student_id = student_id, staff_id = staff_id, review_description = review_description, rating = rating)
+  db.session.add(new_review)
+  db.session.commit()
+  return {"message": "review has been added"}
